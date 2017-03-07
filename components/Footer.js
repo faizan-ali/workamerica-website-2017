8040d4ed20c6903f011202b4002b82c517df0463
kevinmeyvaert <kevin.meyvaert@gmail.com>
2017-03-07 17:32:10 -0500

    :bug: Fixed responsiveness bug in footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
           <div className='row col-lg-12 pb-2'>
             <img src='static/svg/logo.svg' alt='WorkAmerica' className='logo' />
           </div>
-          <div className='row col-lg-6 col-sm-12 pt-4'>
+          <div className='col-lg-6 col-sm-6 pt-4'>
             <ul>
               <li>
                 <Link href='/'><a>Employers</a></Link>
@@ -22,7 +22,7 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className='row col-lg-6 col-sm-12 p-4'>
+          <div className='col-lg-6 col-sm-6 p-4'>
             <ul>
               <li>
                 <a href='http://blog.workamerica.com/' target='_blank'>Blog</a>
@@ -37,12 +37,14 @@ const Footer = () => {
           </div>
         </div>
         <div className='row col-lg-4 newsletter-signup align-items-start justify-content-start'>
-          <h2>Stay up to date!</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <input type='text' placeholder='Email adress' />
-          <button className='submit-newsletter'>Go</button>
+          <h2 className='col-md-12 justify-content-md-center justify-content-lg-start pb-4'>Stay up to date!</h2>
+          <p className='col-md-12 justify-content-md-center justify-content-lg-start pb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <div className='col-md-12 justify-content-md-center justify-content-lg-start pb-4'>
+            <input type='text' placeholder='Email adress' />
+            <button className='submit-newsletter'>Go</button>
+          </div>
         </div>
-        <div className='row col-lg-4 social-icons align-items-center justify-content-end'>
+        <div className='row col-lg-3 col-md-12 col-sm-12 social-icons align-items-center justify-content-lg-end justify-content-md-center justify-content-sm-center'>
           <a href='https://www.linkedin.com/company/workamerica' target='_blank'><i className='fa fa-linkedin-square' aria-hidden='true'></i></a>
           <a href='https://www.facebook.com/workamerica.co/' target='_blank'><i className='fa fa-facebook-square' aria-hidden='true'></i></a>
           <a href='https://twitter.com/work_america' target='_blank'><i className='fa fa-twitter-square' aria-hidden='true'></i></a>
